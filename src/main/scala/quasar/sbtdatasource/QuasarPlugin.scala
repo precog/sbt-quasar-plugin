3ccf306f3e615a13bc8b1462b7617a55f1072c28
Emrys Ingersoll <ingersoll@gmail.com>
2019-09-11 09:42:34 -0500

    Add destination support, rename to QuasarPlugin

---
+++ src/main/scala/quasar/sbtdatasource/QuasarPlugin.scala
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2014–2019 SlamData Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package quasar.sbtdatasource
+
+import scala.{None, Option}
+import scala.Predef.String
+import scala.collection.Seq
+import scala.concurrent.ExecutionContext
+
+import cats.effect.IO
+import coursier.interop.cats._
+import sbt._, Keys._
+
+object QuasarPlugin extends AutoPlugin {
+
+  private implicit val globalContextShift =
+    IO.contextShift(ExecutionContext.Implicits.global)
+
+  object autoImport {
+    val quasarPluginName: SettingKey[String] = settingKey[String]("The short name of the plugin (e.g. 's3', 'azure').")
+    val quasarPluginDependencies: SettingKey[Seq[ModuleID]] = settingKey[Seq[ModuleID]]("Declares the non-quasar managed dependencies.")
+    val quasarPluginDatasourceFqcn: SettingKey[Option[String]] = settingKey[Option[String]]("The fully qualified class name of the datasource module.")
+    val quasarPluginDestinationFqcn: SettingKey[Option[String]] = settingKey[Option[String]]("The fully qualified class name of the destination module.")
+    val quasarPluginQuasarVersion: SettingKey[String] = settingKey[String]("Defines the version of Quasar to depend on.")
+
+    val quasarPluginAssemble: TaskKey[File] = taskKey[File]("Produces the tarball containing the quasar plugin and all non-quasar dependencies.")
+  }
+
+  import autoImport._
+
+  override def requires = plugins.JvmPlugin
+
+  override def projectSettings = {
+    val srcFqcn = quasarPluginDatasourceFqcn
+    val dstFqcn = quasarPluginDestinationFqcn
+
+    Seq(
+      quasarPluginDependencies := Seq.empty,
+
+      quasarPluginDatasourceFqcn := None,
+
+      quasarPluginDestinationFqcn := None,
+
+      libraryDependencies := {
+        val quasarDependencies =
+          (srcFqcn.value.isDefined, dstFqcn.value.isDefined) match {
+            case (true, _) =>
+              Seq(
+                "com.slamdata" %% "quasar-connector" % quasarPluginQuasarVersion.value,
+                "com.slamdata" %% "quasar-connector" % quasarPluginQuasarVersion.value % Test classifier "tests")
+
+            case (_, true) =>
+              Seq("com.slamdata" %% "quasar-api" % quasarPluginQuasarVersion.value)
+
+            case _ =>
+              Seq()
+          }
+
+        libraryDependencies.value ++ quasarPluginDependencies.value ++ quasarDependencies
+      },
+
+      packageOptions in (Compile, packageBin) += {
+        val attrs =
+          srcFqcn.value.toList.map(("Datasource-Module", _)) ++
+          dstFqcn.value.toList.map(("Destination-Module", _))
+
+        Package.ManifestAttributes(attrs: _*),
+      },
+
+      quasarPluginAssemble := {
+        val pluginPath =
+          AssemblePlugin[IO, IO.Par](
+            quasarPluginName.value,
+            version.value,
+            quasarPluginDependencies.value,
+            (Keys.`package` in Compile).value.toPath,
+            quasarPluginQuasarVersion.value,
+            (scalaBinaryVersion in Compile).value,
+            (crossTarget in Compile).value.toPath)
+
+        pluginPath.map(_.toFile).unsafeRunSync()
+      })
+  }
+}
